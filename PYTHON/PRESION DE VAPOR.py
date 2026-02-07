795fe14a0da79a20c5e6cfb66e8d1d2535d1c0a2
rocio <vanessarociosorianoll@gmail.com>
2026-02-06 21:35:10 -0500

    Update PRESION DE VAPOR.py

--- PYTHON/PRESION DE VAPOR.py
+++ PYTHON/PRESION DE VAPOR.py
@@ -18,7 +18,7 @@ with col1:
     st.image("datos vap.png", caption="Modelo de Excel requerido",width=200)
 with col2:
     st.write("**Sube tu archivo :D**")
-    uploaded_file = st.file_uploader("Elige tu archivo de Excel", type=["xlsx", "xls", "csv"])
+    uploaded_file = st.file_uploader(type=["xlsx", "xls", "csv"])
     if uploaded_file is not None:
         df1=pd.read_excel(excel, sheet_name=0)
         lista_temperatura=df1["T(℃)"].tolist()
@@ -116,6 +116,7 @@ with col2:
         A=e**b
 
         st.latex(fr"P = e^{{ \frac{{ -{deltaHV:.4f} }}{{ {R} \cdot T }} }} \cdot {A:.4f}")
+
 
 
 
