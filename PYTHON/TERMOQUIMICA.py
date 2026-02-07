1c0e0b72e7d8445174fe9cccadf885d2b566487e
rocio <vanessarociosorianoll@gmail.com>
2026-02-06 23:04:27 -0500

    Update TERMOQUIMICA.py

--- PYTHON/TERMOQUIMICA.py
+++ PYTHON/TERMOQUIMICA.py
@@ -208,7 +208,7 @@ if va>0:
                 df3.at[i,"CONCENTRACION CORREGIDA(N)"]=round(Acido,4)
             if ct1>0:
                 error2=abs(ct1-Acido)*100/ct1
-                df3.at[i,"%_ERROR"]=round(error2,4)
+                df3.at[i,"%_ERROR"]=round(error2,2)
             else:
                 df3.at[i,"%_ERROR"]=0.0
 
@@ -275,9 +275,10 @@ if va>0:
               E = -(Q1_J / 1000) / n_Nacl
               st.success(f"△Hrnx= {float(E):.4f} KJ/mol")
               erorrorr=abs(-55.8-E)*100/55.8
-              st.error(f"%Error={erorrorr} %")
+              st.error(f"%Error={erorrorr:.2f} %")
           
               st.info("En el cálculo exacto, no se asumió una  solución ideal. Se utilizó un modelo de Capacidad Calorífica Molar Aparente basado en el CRC Handbook. El programa calculó la masa efectiva del solvente restando la masa de los solutos y aplicó las contribuciones térmicas individuales de cada especie ($NaCl, NaOH$ y $H_2O$). Esto permite capturar el efecto de la interacción ion-solvente que reduce la capacidad calorífica del sistema, entregando una entalpía de neutralización basada en la física real de la mezcla.Siendo este calculo , mucho mas sensible en comparacion al simple :D")
+
 
 
 
