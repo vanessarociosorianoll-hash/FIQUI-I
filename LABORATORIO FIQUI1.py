e533108c4df726d78fa4981990e0373dbbdc175a
rocio <vanessarociosorianoll@gmail.com>
2026-02-07 21:51:11 -0500

    Update LABORATORIO FIQUI1.py

--- LABORATORIO FIQUI1.py
+++ LABORATORIO FIQUI1.py
@@ -2,14 +2,14 @@ import streamlit as st
 import sys
 import os
 
-# 1. Agregamos la carpeta PYTHON al buscador de módulos
-# Esto es vital para que tus apps encuentren la carpeta Handbook
+
 ruta_python = os.path.join(os.path.dirname(__file__), "PYTHON")
 if ruta_python not in sys.path:
     sys.path.insert(0, ruta_python)
+st.sidebar.title("LABO FIQUI I - FQIQ")
+st.sidebar.markdown("---")
 
-# 2. Definimos las páginas (Quitamos el import * que daba error)
-pagina_0 = st.Page("PYTHON/GASES-FSQI.py", title="GASES", icon="🎈")
+pagina_0 = st.Page("PYTHON/GASES-FSQI.py", title="GASES", icon="")
 pagina_1 = st.Page("PYTHON/PRESION DE VAPOR.py", title="PRESIÓN DE VAPOR", icon="🌡️")
 pagina_2 = st.Page("PYTHON/TERMOQUIMICA.py", title="TERMOQUÍMICA", icon="🔥")
 pagina_3 = st.Page("PYTHON/CRIOSCOPIA.py", title="CRIOSCOPÍA", icon="❄️")
@@ -17,8 +17,9 @@ pagina_5 = st.Page("PYTHON/REFRAC.py", title="REFRACTOMETRÍA", icon="🔍")
 pagina_6 = st.Page("PYTHON/TENSION SUPERFICIAL.py", title="TENSIÓN SUPERFICIAL", icon="💧")
 pagina_7=st.Page("PYTHON/VISCOSIDAD.py", title="VISCOSIDAD", icon="🫗")
 
-# 3. Navegación
+
 pg = st.navigation([pagina_0, pagina_1, pagina_2, pagina_3, pagina_5, pagina_6,pagina_7])
 
 st.set_page_config(page_title="Laboratorio de Fisicoquímica - UNMSM", layout="wide")
+
 pg.run()
